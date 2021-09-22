7d53a253b2205941cc6ce43c33ff358e1a58eeb4
mainmgalaktionov <91167500+mainmgalaktionov@users.noreply.github.com>
2021-09-22 04:29:13 +0300

    Add files via upload

--- 4sem/2/main.py
+++ 4sem/2/main.py
@@ -1,17 +1,47 @@
-   
-import time
+import functools
 
-def time_of_function(function):
-    def wrapped(*args):
-        start_time = time.perf_counter_ns()
-        res = function(*args)
-        print(time.perf_counter_ns() - start_time)
-        return res
-    return wrapped
+def decorator(func):
+    @functools.wraps(func)
+    def wrap(*args):
+        f = open('output.log', 'w')
+        result = func(*args)
+        name = ''
+        if (args[2] == '+'):
+            name = 'Сложение'
+        elif (args[2] == '-'):
+            name = 'Вычитание'
+        elif (args[2] == '*'):
+            name = 'Умножение'
+        elif (args[2] == '/'):
+            name = 'Деление'
+        else:
+          return "Неизвестное действие"
+        f.write(f"{args[0]}, {args[1]}, {name}, {result}")
+        f.close()
+        return result
+    return wrap
 
-@time_of_function
-def func(first, second):
-    return first + second
+def calc(a,b,act):
+    res = 0
+    if (act == '+'):
+        res = a+b
+    if (act == '-'):
+        res = a-b
+    if (act == '*'):
+        res = a*b
+    if (act == '/'):
+        res = a/b
+    return res
 
-
-print(func("111", "0000"))
+    
+def main():
+    a = int(input("Первое число:"))
+    b = int(input("Второе число:"))
+    act = str(input("Введите действие:"))
+    v = decorator(calc)
+    
+    ans = v(a,b,act)
+    print(ans)
+    pass
+                                    
+main()    
